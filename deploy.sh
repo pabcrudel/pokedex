765f68fb89d2cc1cf8cbf6d5a266076f2f3ac862
pabcrudel <pabcrudel@gmail.com>
2023-02-13 18:37:11 +0100

    Created deploy insntructions

---
+++ deploy.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+#
+# Script to automate the deploy on github pages
+#
+# --------------------------------------------------------------------
+# Author: Pablo Cru
+# GitHub: https://github.com/pabcrudel
+# --------------------------------------------------------------------
+
+set -e
+
+# Build
+npm run build
+
+# Save the latest commit hash as a string
+LOGSTRING=$(git log)
+COMMIT=$(echo $LOGSTRING | awk '{print $2}')
+
+# Navigate into the build output directory
+cd dist
+
+git init 
+git add -A
+git commit -m "Deploy (commit: $COMMIT)"
+
+git push -f https://github.com/pabcrudel/pokedex.git master:pg-pages
+
+cd -
