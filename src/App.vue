34773d4536fbf0d620b03603228c1fcda5086754
pabcrudel <pabcrudel@gmail.com>
2023-02-16 08:55:45 +0100

    Created arrows

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,10 @@
 <template>
   <h1>Pokedex</h1>
   <p>This will display pokemons from Gen1!</p>
+  <i class="material-symbols-outlined" v-html="'expand_less'" id="arrowKeyUp"/>
+  <i class="material-symbols-outlined" v-html="'chevron_right'" id="arrowKeyRight"/>
+  <i class="material-symbols-outlined" v-html="'chevron_left'" id="arrowKeyLeft"/>
+  <i class="material-symbols-outlined" v-html="'expand_more'" id="arrowKeyDown"/>
 </template>
 
 <script setup>
@@ -8,4 +12,6 @@ import { useHead } from '@unhead/vue';
 useHead({  
   title: 'Pokedex'
 });
+
+
 </script>
