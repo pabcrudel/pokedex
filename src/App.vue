2281c1bf696486e388f5a0ac428962452041e188
pabcrudel <pabcrudel@gmail.com>
2023-02-19 21:14:47 +0100

    Arrow Keys are detected when they are pressed

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,20 @@
 <template>
   <h1>Pokedex</h1>
   <p>This will display pokemons from Gen1!</p>
-  <i class="material-symbols-outlined" v-html="'expand_less'" id="arrowKeyUp"/>
-  <i class="material-symbols-outlined" v-html="'chevron_right'" id="arrowKeyRight"/>
-  <i class="material-symbols-outlined" v-html="'chevron_left'" id="arrowKeyLeft"/>
-  <i class="material-symbols-outlined" v-html="'expand_more'" id="arrowKeyDown"/>
+  <i class="material-symbols-outlined" :class="{ isPressed: ArrowUp }" v-html="'expand_less'" id="arrowKeyUp"/>
+  <i class="material-symbols-outlined" :class="{ isPressed: ArrowRight }" v-html="'chevron_right'" id="arrowKeyRight"/>
+  <i class="material-symbols-outlined" :class="{ isPressed: ArrowLeft }" v-html="'chevron_left'" id="arrowKeyLeft"/>
+  <i class="material-symbols-outlined" :class="{ isPressed: ArrowDown }" v-html="'expand_more'" id="arrowKeyDown"/>
 </template>
 
 <script setup>
-import { useHead } from '@unhead/vue';
-useHead({  
-  title: 'Pokedex'
-});
+import { useMagicKeys } from '@vueuse/core'
 
-
+const {ArrowUp, ArrowDown, ArrowLeft, ArrowRight} = useMagicKeys()
 </script>
+
+<style scoped>
+.isPressed {
+  color: white;
+}
+</style>
